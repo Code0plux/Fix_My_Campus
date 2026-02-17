e5651ca51228a32e66ce6ec6fe4036e780412831
Code0plux <darkdemanders@gmail.com>
2026-02-17 14:30:27 +0530

    added screen and auth

---
+++ lib/Screen/complaint_register.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ComplaintRegister extends StatefulWidget {
+  const ComplaintRegister({super.key});
+
+  @override
+  State<ComplaintRegister> createState() => _ComplaintRegisterState();
+}
+
+class _ComplaintRegisterState extends State<ComplaintRegister> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
